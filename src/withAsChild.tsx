863ab388680a55724c32cd07a9babdc0602de6e8
codebender828 <codebender828@gmail.com>
2023-04-28 04:04:53 +0800

    chore: cleanup

--- src/withAsChild.tsx
+++ src/withAsChild.tsx
@@ -1,11 +1,4 @@
-import {
-  type PropType,
-  defineComponent,
-  h,
-  type Slots,
-  isVNode,
-  type VNode,
-} from "vue";
+import { type PropType, defineComponent, h } from "vue";
 
 export type AsChildProps = {
   asChild?: boolean;
@@ -13,25 +6,13 @@ export type AsChildProps = {
 
 type RenderFunctionArgs = Parameters<typeof h>[0];
 
-/**
- * Gets only the valid children of a component,
- * and ignores any nullish or falsy child.
- */
-export function getValidChildren(slots: Slots | null): VNode[] {
-  const slotArray = slots?.default?.() || [];
-  return slotArray.filter((child) => {
-    return isVNode(child);
-  });
-}
-
 export function withAsChild(__component: RenderFunctionArgs) {
   const component = defineComponent<AsChildProps>(
     ({ asChild }, { attrs, slots }) => {
       if (!asChild)
         return () => <__component {...attrs}>{slots.default?.()}</__component>;
       else {
-        console.log("attrs", attrs);
-        return () => slots.default?.(attrs); //  {
+        return () => slots.default?.(attrs);
       }
     }
   );
